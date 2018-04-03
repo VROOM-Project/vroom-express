85b4c2542ee17111ccb88208af0d39762f9bfa2f
jcoupey <git@coupey.fr>
2018-04-03 18:37:27 +0200

    Code cleanup.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var helmet = require('helmet');
 var minimist = require('minimist');
 
 // Config variables.
-let args = minimist(process.argv.slice(2), {
+var args = minimist(process.argv.slice(2), {
   alias: {
     p: 'port',
   },
@@ -18,80 +18,68 @@ let args = minimist(process.argv.slice(2), {
     'override'
   ],
   default: {
-    port: 3000,
-    path: '',
-    maxjobs: '1000',
-    geometry: false,
-    libosrm: false,
+    port: 3000,                 // expressjs port
+    path: '',                   // VROOM path (if not in $PATH)
+    maxjobs: '1000',            // max number of jobs
+    geometry: false,            // retrieve geometry (-g)
+    libosrm: false,             // use libosrm (-l)
     osrm_address: "0.0.0.0",
     osrm_port: 5000,
-    override: true, // -g only so far.
-    logdir: __dirname + '/..',
-    limit: '500kb',
-    timeout: 5 * 60 * 1000 // milli-seconds.
+    override: true,             // allow cl option override (-g only so far)
+    logdir: __dirname + '/..',  // put logs in there
+    sizelimit: '500kb',         // max request size
+    timeout: 5 * 60 * 1000      // milli-seconds.
   }
 });
-
-var PORT = args['port'];
-var VROOM_PATH = args['path'];
-var MAX_JOB_NUMBER = args['maxjobs'];
-var ROUTE_GEOMETRY = args['geometry'];
-var USE_LIBOSRM = args['libosrm'];
-var OSRM_ADDRESS = args['osrm_address'];
-var OSRM_PORT = args['osrm_port'];
-var ALLOW_OPTIONS_OVERRIDE = args['override']; // -g only so far.
-var LOG_DIRNAME = args['logdir'];
-var INPUT_SIZE_LIMIT = args['limit'];
-var CUSTOM_TIMEOUT = args['timeout']; // milli-seconds.
-
 
 // App and loaded modules.
 var app = express();
 
 // Enable cross-origin ressource sharing.
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers",
              "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use(bodyParser.json({limit: INPUT_SIZE_LIMIT}));
-app.use(bodyParser.urlencoded({limit: INPUT_SIZE_LIMIT, extended: true}));
+app.use(bodyParser.json({limit: args['limit']}));
+app.use(bodyParser.urlencoded({limit: args['limit'], extended: true}));
 
-var accessLogStream = fs.createWriteStream(LOG_DIRNAME + '/access.log',
+var accessLogStream = fs.createWriteStream(args['logdir'] + '/access.log',
                                            {flags: 'a'});
+
 app.use(morgan('combined', {stream: accessLogStream}));
 
 app.use(helmet());
 
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
   res.setHeader('Content-Type', 'application/json');
-  if(err instanceof SyntaxError && err.status === 400 && 'body' in err){
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
     console.log(now() + ' - ' + 'Invalid JSON');
     res.send({code: 1, error: 'Invalid json.'});
   }
 });
 
 // Simple date generator for console output.
-var now = function(){
+var now = function() {
   var date = new Date();
   return date.toUTCString();
 }
 
 // Callback for size and some input validity checks.
-var sizeCheckCallback = function(maxJobNumber){
-  return function (req, res, next){
+var sizeCheckCallback = function(maxJobNumber) {
+  return function (req, res, next) {
     var correctInput = ('jobs' in req.body)
         && ('vehicles' in req.body)
         && (req.body['vehicles'].length >= 1);
 
-    if(!correctInput){
+    if (!correctInput) {
       res.send({code: 1, error: 'Invalid query.'});
       return;
     }
 
-    if(req.body['jobs'].length > maxJobNumber){
+    if (req.body['jobs'].length > maxJobNumber) {
       console.log(now()
                   + ' - Too many jobs in query ('
                   + req.body['jobs'].length + ')');
@@ -105,16 +93,15 @@ var sizeCheckCallback = function(maxJobNumber){
 // Cli wrapper and associated callback.
 var spawn = require('child_process').spawn;
 
-var vroomCommand = VROOM_PATH + 'vroom';
+var vroomCommand = args['path'] + 'vroom';
 var options = [];
-if(USE_LIBOSRM){
+if (args['libosrm']) {
   options.push('-l');
+} else {
+  options.push('-a', args['osrm_address']);
+  options.push('-p', args['osrm_port']);
 }
-else{
-  options.push('-a', OSRM_ADDRESS);
-  options.push('-p', OSRM_PORT);
-}
-if(ROUTE_GEOMETRY){
+if (args['geometry']) {
   options.push('-g');
 }
 
@@ -122,33 +109,33 @@ if(ROUTE_GEOMETRY){
 // should be made into a variable.
 options.push('-m', 'car');
 
-var execCallback = function (req, res){
+var execCallback = function (req, res) {
   var reqOptions = options.slice();
-  if(!ROUTE_GEOMETRY && ALLOW_OPTIONS_OVERRIDE
+  if (!args['geometry'] && args['override']
      && 'options' in req.body && 'g' in req.body['options']
-     && req.body['options']['g']){
+     && req.body['options']['g']) {
     reqOptions.push('-g');
   }
 
   reqOptions.push(JSON.stringify(req.body));
   var vroom = spawn(vroomCommand, reqOptions);
 
-  vroom.on('error', function(err){
+  vroom.on('error', function(err) {
     console.log(now() + ' - ' + err);
     res.send({code: 1, error: 'Unfound command: ' + vroomCommand});
   });
 
   vroom.stdout.pipe(res);
 
-  vroom.stderr.on('data', function (data){
+  vroom.stderr.on('data', function (data) {
     console.log(now() + ' - ' + data.toString());
   });
 }
 
-app.post('/', [sizeCheckCallback(MAX_JOB_NUMBER), execCallback]);
+app.post('/', [sizeCheckCallback(args['maxjobs']), execCallback]);
 
-var server = app.listen(PORT, function (){
-  console.log('vroom-express listening on port ' + PORT + '!');
+var server = app.listen(args['port'], function () {
+  console.log('vroom-express listening on port ' + args['port'] + '!');
 });
 
-server.setTimeout(CUSTOM_TIMEOUT);
+server.setTimeout(args['timeout']);
